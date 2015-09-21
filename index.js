a6031ef56ca9998fa9816c20bce48f39ab1f4154
nomilous <nomilous@gmail.com>
2015-09-21 09:22:39 +0200

    late module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.AsFactory = require('./lib/as_factory');
 module.exports.AsModule = require('./lib/as_module');
 module.exports.AsClass = require('./lib/as_class');
+module.exports.AsLate = require('./lib/as_late');
