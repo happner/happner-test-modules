7ee1eca87185128eca2d39b468a3e508a8c31460
nomilous <nomilous@gmail.com>
2015-09-23 21:59:41 +0200

    0.0.2

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
+module.exports = Default;
+
 module.exports.AsFactory = require('./lib/as_factory');
 module.exports.AsModule = require('./lib/as_module');
 module.exports.AsClass = require('./lib/as_class');
 module.exports.AsLate = require('./lib/as_late');
+
+
+function Default(arg, u, ments) {
+  this.args = arg + u + ments;
+  this.started = false;
+};
+
+
+Default.prototype.start = function($happn, callback) {
+  this.started = true;
+  callback();
+}
+
+Default.prototype.stop = function($happn, callback) {
+  this.started = false;
+  callback();
+}
+
+
+
+Default.prototype.exchangeMethod = function(opts, callback) {
+  opts.args = this.args;
+  opts.started = this.started;
+  callback(null, opts);
+}
+
+
+Default.prototype.mware1 = function($happn, res, req, next) {
+  res.send('mware1\n');
+  next();
+}
+
+Default.prototype.mware2 = function(res, req, next) {
+  res.send('mware2\n');
+  next();
+}
+
+Default.prototype.webMethod = function($happn, res, req, next) {
+  res.end('done');
+}
+
+Default.prototype.$happner = {
+  config: {
+    component: {
+      startMethod: 'start',
+      stopMethod: 'stop',
+      web: {
+        routes: {
+          'long-route': ['mware1', 'mware2', 'webMethod'],
+          'short-route': 'webMethod',
+          'widget': 'static',
+          'static': 'static',
+        }
+      }
+    }
+  }
+}
